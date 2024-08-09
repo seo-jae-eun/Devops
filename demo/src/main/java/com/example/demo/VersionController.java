0eabdd6ddd16f455858ce7ae01050c18c6a45248
seo-jae-eun <wodms5522@naver.com>
2024-08-09 21:02:51 +0900

    [Test] 푸시 테스트

---
+++ demo/src/main/java/com/example/demo/VersionController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+    @RequestMapping(method = RequestMethod.GET, value = "/version")
+    public ResponseEntity<String> getVersion() {
+        return ResponseEntity.ok("version 1.0.0");
+    }
+}
